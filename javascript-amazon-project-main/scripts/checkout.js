ae9d6c28ea064761554584457236c135a5547145
shiv prasad <shivprasadshivadasnair@gmail.com>
2024-06-16 14:57:49 +0530

    delect product from clicking delect

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -19,7 +19,8 @@ cart.forEach((cartItem)=>{
     })
     cartSummaryHTML+=
 `
-<div class="cart-item-container">
+<div class="cart-item-container
+js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -109,6 +110,11 @@ document.querySelectorAll('.js-dele-link')
   link.addEventListener('click',()=>{
     const productId=link.dataset.productId;
  removeFromCart(productId);
+
+  const container = document.querySelector
+ (`.js-cart-item-container-${productId}`)
+
+ container.remove();
 
   })
 
